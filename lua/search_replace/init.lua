1b31afa3bb88d60b921ec2194fa333576754e6d9
saltchicken <john.eicher89@gmail.com>
2026-05-13 12:38:32 -0700

    No longer need search_replace.lua file or plugin folder

--- lua/search_replace/init.lua
+++ lua/search_replace/init.lua
@@ -1,7 +1,13 @@
 local M = {}
 
 M.setup = function(opts)
-	M.config = vim.tbl_deep_extend("force", {}, opts or {})
+	M.config = vim.tbl_deep_extend("force", {
+		-- defaults go here
+	}, opts or {})
+
+	vim.api.nvim_create_user_command("SearchReplace", function()
+		require("search_replace.ui").open_input()
+	end, { desc = "Open Search Replace UI" })
 end
 
 return M
--- plugin/search_replace.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_create_user_command("SearchReplace", function()
-	require("search_replace.ui").open_input()
-end, {})
