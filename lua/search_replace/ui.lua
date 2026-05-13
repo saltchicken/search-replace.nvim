ffa257885c1a4fcd3a006b321be50d359e7ff1f9
saltchicken <john.eicher89@gmail.com>
2026-05-13 12:59:17 -0700

    Fix UI default

--- lua/search_replace/ui.lua
+++ lua/search_replace/ui.lua
@@ -1,23 +1,13 @@
 local M = {}
-local logic = require("search_replace.logic")
+local logic = require("search-replace.logic")
 
 function M.open_input()
 	local buf = vim.api.nvim_create_buf(false, true)
-
-	-- Ensure the buffer cleans up after itself
-	vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf })
-	vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf })
-
 	local width = vim.api.nvim_get_option_value("columns", {})
 	local height = vim.api.nvim_get_option_value("lines", {})
 	local win_width = math.floor(width * 0.8)
 	local win_height = math.floor(height * 0.8)
 
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, {
-		"-- PASTE SEARCH/REPLACE BLOCKS --",
-		"-- <CR> to Apply | q to Cancel --",
-		"",
-	})
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, {
 		"-- PASTE SEARCH/REPLACE BLOCKS --",
 		"-- <CR> to Apply | q to Cancel --",
@@ -36,11 +26,7 @@ function M.open_input()
 		title_pos = "center",
 	})
 
-	-- Put cursor inside the SEARCH block
 	vim.api.nvim_win_set_cursor(win, { 3, 0 })
-
-	-- Automatically start in insert mode to make pasting faster
-	vim.cmd("startinsert")
 
 	vim.keymap.set("n", "<CR>", function()
 		local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
